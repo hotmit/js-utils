e55d0d1874e58c7ccf257ed2e138f0ea24894880
Du Dang <hotmit@gmail.com>
2015-10-18 22:44:59 -0400

    added gm script

--- style.js
+++ style.js
@@ -1,21 +1,19 @@
 /*global jQuery */
 
-// STANDALONE: pure js
+// STANDALONE: jq
 
 
 (function (global, $) {
 	"use strict";
 
 	(function (Stl) {
-
         /**
          * Add the style to the head (string -> css style text)
          * @param style {String}
          */
         Stl.add = function(style){
-            $('<style type="text/css"> ' + style + ' </style>').appendTo('head');
+            $('<style type="text/css">' + style + '</style>').appendTo('head');
         };
-
 	}(global.Stl));
 
 }(typeof window !== 'undefined' ? window : this, jQuery));
