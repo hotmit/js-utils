cac37613f98ad5b071c46f8ebc095feafbd0fb07
Du Dang <ddang@infitrak.com>
2016-03-08 20:40:48 -0500

    add style using array instead of str

--- style.js
+++ style.js
@@ -9,10 +9,26 @@
 	(function (Stl) {
         /**
          * Add the style to the head (string -> css style text)
-         * @param style {String}
+         * @param style {String|Array} - style text, http link or array of links
          */
         Stl.add = function(style){
-            $('<style type="text/css">' + style + '</style>').appendTo('head');
+            if ($.isArray(style))
+            {
+                $.each(style, function(i, elm){
+                    $('<link href="">').attr('href', elm).appendTo('head');
+                });
+            }
+            else if ($.type(style) === 'string')
+            {
+                if (style.indexOf('http') == 0)
+                {
+                    Stl.add([style]);
+                }
+                else
+                {
+                    $('<style type="text/css">' + style + '</style>').appendTo('head');
+                }
+            }
         };
 	}(global.Stl));
 
